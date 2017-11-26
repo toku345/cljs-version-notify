c4c3648071d123dc41f38eaa982bc7769ea34d84
Fumitaka Tokumitsu <toku345@gmail.com>
2017-11-26 19:46:38 +0900

    Create CljsVersionIntent handler function

--- src/cljs_version_notify/core.cljs
+++ src/cljs_version_notify/core.cljs
@@ -3,6 +3,33 @@
 
 (nodejs/enable-util-print!)
 
+(defonce Alexa (nodejs/require "alexa-sdk"))
+
+(def cljs-latest-version "1.9.946") ; TODO: I want to get it dynamically...
+
+
+(def skill-handlers #js {"LaunchRequest"     (fn [] (this-as this (.emit this "AMAZON.HelpIntent")))
+                         "AMAZON.HelpIntent" (fn [] (this-as this
+                                                      (.emit this
+                                                             ":tell"
+                                                             (str "cljsの最新バージョン番号をお知らせします。"
+                                                                  "たとえば、バージョンチェックでcljsのバージョンを教えてと聞いてください。"))))
+                         "CljsVersionIntent" (fn [] (this-as this
+                                                      (let [message (str "最新のcljsのバージョンは" cljs-latest-version "です。")]
+                                                        (.emit this ":tell" message)
+                                                        (.log js/console (str "message: " message)))))})
+
+(defn- handler [event context callback]
+  (let [alexa (.handler Alexa event context)
+        app-id (-> nodejs/process .-env .-APP_ID)]
+    (set! (.-appId alexa) app-id)
+    (.log js/console (str "app-id: " app-id))
+    (.registerHandlers alexa skill-handlers)
+    (.execute alexa)))
+
+(set! (.-exports js/module) #js {:handler handler})
+
+
 (defn -main []
   (println "Hello world!"))
 
