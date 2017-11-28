ebb168afc7f7f5c8e231fa5696b3690132c52e2f
Fumitaka Tokumitsu <toku345@gmail.com>
2017-11-28 19:02:58 +0900

    Update response message

--- src/cljs_version_notify/core.cljs
+++ src/cljs_version_notify/core.cljs
@@ -6,14 +6,14 @@
 (defonce Alexa (nodejs/require "alexa-sdk"))
 
 (def ^:const cljs-latest-version "1.9.946") ; TODO: I want to get it dynamically...
-(def ^:const stop-message "さようなら。")
+(def ^:const stop-message "クロージャースクリプトの最新バージョン番号が気になったら、また声をかけてくださいね。")
 
 (def skill-handlers #js {"LaunchRequest"       (fn [] (this-as this (.emit this "AMAZON.HelpIntent")))
                          "SessionEndedRequest" (fn [] (this-as this (.emit this "AMAZON.StopIntent")))
                          "AMAZON.HelpIntent"   (fn [] (this-as this
                                                         (.emit this
                                                                ":ask"
-                                                               (str "クロージャースクリプトの最新バージョンお知らせする非公式のスキルです。"
+                                                               (str "クロージャースクリプトの最新バージョン番号をお知らせする非公式のスキルです。"
                                                                     "クロージャースクリプトの最新バージョン番号をお知らせしましょうか？"))))
                          "CljsVersionIntent"   (fn [] (this-as this
                                                         (let [message (str "最新のcljsのバージョンは" cljs-latest-version "です。")]
